e5a3e2df9df057c2d1b34713b4112c1f627d8809
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-02 17:46:10 -0300

    delete funcionando

--- Teste2API/Controllers/TodoController.cs
+++ Teste2API/Controllers/TodoController.cs
@@ -60,5 +60,20 @@ namespace Teste2API.Controllers {
 
             return NoContent();
         }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id) {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null) {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
