c0e3088d64a0aec1a953cd64d41dc0e9a4ab34a9
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-02 18:04:51 -0300

    TO-DO funcionando completamente

--- Teste2API/Startup.cs
+++ Teste2API/Startup.cs
@@ -37,6 +37,8 @@ namespace Teste2API
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
