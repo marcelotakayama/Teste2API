b518c8f3db6390ec24db3c1d2b410fcfc1453868
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-02 15:04:51 -0300

    first commit

---
+++ Teste2API/Controllers/TodoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Teste2API.Models;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Teste2API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context) {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0) {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItem() {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id) {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null) {
+                return NotFound();
+            }
+            return todoItem;
+        }
+    }
+}
---
+++ Teste2API/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teste2API.Models {
+    public class TodoContext : DbContext {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
